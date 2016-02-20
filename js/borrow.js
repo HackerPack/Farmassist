821bfcfc6aae3753be1f58a2f540256f6033c3f9
Lakshman Naresh C A <lakshmanaresh@gmail.com>
2016-02-20 17:27:55 -0600

    Completed login changes

--- js/borrow.js
+++ js/borrow.js
@@ -1,24 +1,26 @@
 $(function(){
 
-	function showBooks(query){
+	function showEquipments(query){
 		if(query){
 			query = query.trim();
 		}
 
-		searchBook(query, function(data){
-			var renderedHTML = renderBooks(data);
-			$("#books").html(renderedHTML);
+		searchEquipment(query, function(data){
+			var renderedHTML = renderEquipments(data);
+			$("#equipments").html(renderedHTML);
 
 			$(".btn-success").click(function(){
 				if($(this).html() != "Rent it!"){
 					return;
 				}
-				var isbn = $(this).data("id");
+				var equipmentId = $(this).data("id");
+				alert("Equ id is " + equipmentId);
+				return;
 				$(this).removeClass('btn-success');
 				$(this).addClass('btn-danger');
 				$(this).html("Renting");
 				var that = this;
-				borrow_book(authData.uid, isbn, function(){
+				borrow_equipment(authData.uid, equipmentId, function(){
 					$(that).html("Done");
 				});
 			})
@@ -26,10 +28,10 @@ $(function(){
 	}
 
 	$("#search").change(function(){
-		showBooks($(this).val());
+		showEquipments($(this).val());
 	});
 
-	showBooks();
+	showEquipments();
 
 	$("#list_mode").click(function(){
 		$(".list_content").show();
@@ -74,15 +76,15 @@ function loadMap(){
 		L.marker([position.coords.latitude, position.coords.longitude], {
 	        icon: icons.marker
 	    }).addTo(map);
-		searchBook(null, function(books){
-			for(var i=0; i<books.length; i++){
-				if(books[i].status == 0){
-					L.marker([books[i].lat, books[i].lon], {
+		searchEquipment(null, function(equipments){
+			for(var i=0; i<equipments.length; i++){
+				if(equipments[i].status == 0){
+					L.marker([equipments[i].lat, equipments[i].lon], {
 				        icon: icons.red
 				    }).addTo(map);
 				}
 				else{
-					L.marker([books[i].lat, books[i].lon], {
+					L.marker([equipments[i].lat, equipments[i].lon], {
 				        icon: icons.green
 				    }).addTo(map);
 				}
@@ -92,15 +94,15 @@ function loadMap(){
 }
 
 
-function renderBooks(books){
+function renderEquipments(equipments){
 	var renderedHTML = "<div class='list-group'>";
-	for(var i=0; i<books.length; i++){
-		if(books[i].status == 1){
+	for(var i=0; i<equipments.length; i++){
+		if(equipments[i].status == 1){
 			var addressString = null;
-			if(books[i].lat != null && books[i].lon != null){
+			if(equipments[i].lat != null && equipments[i].lon != null){
 				 $.ajax({
 	        		type: "GET",
-	        		url: "http://maps.googleapis.com/maps/api/geocode/json?latlng="+books[i].lat+","+books[i].lon,
+	        		url: "http://maps.googleapis.com/maps/api/geocode/json?latlng="+equipments[i].lat+","+equipments[i].lon,
 	        		async: false,
 	        		success : function(data) {
 	        			addressString = data.results[0].formatted_address;
@@ -110,19 +112,19 @@ function renderBooks(books){
     			});
 			}
 			var status = "<span class='label label-success' style='margin-left:10px;'>Available</span>";
-			var btn = "<button class='btn btn-success pull-right' data-id='" + books[i].isbn +"''>Rent it!</button>";
-			var newHTML = "<a class='list-group-item book'><h4 class='title'>"+books[i].name+status+"</h4>";
+			var btn = "<button class='btn btn-success pull-right' data-id='" + equipments[i].equipmentId +"''>Rent it!</button>";
+			var newHTML = "<a class='list-group-item book'><h4 class='title'>"+equipments[i].name+status+"</h4>";
 			if(addressString){
-				newHTML = newHTML+"<p class='author'><b>Address</b>&nbsp;&nbsp;"+addressString+"</p>";
+				newHTML = newHTML+"<p class='desc'><b>Address</b>&nbsp;&nbsp;"+addressString+"</p>";
 			}	
-			newHTML = newHTML+"<p class='author'><b>Author</b>&nbsp;&nbsp;"+books[i].author+btn+"</p><div class='category'><b>Categories</b>&nbsp;&nbsp;"+books[i].catagory+"</div></a>";
+			newHTML = newHTML+"<p class='desc'><b>Description</b>&nbsp;&nbsp;"+equipments[i].description+btn+"</p><div class='category'><b>Categories</b>&nbsp;&nbsp;"+equipments[i].catagory+"</div></a>";
 			renderedHTML += newHTML;
 		}
 	}
-	for(var i=0; i<books.length; i++){
-		if(books[i].status == 0){
+	for(var i=0; i<equipments.length; i++){
+		if(equipments[i].status == 0){
 			var status = "<span class='label label-danger' style='margin-left:10px;'>Taken</span>";
-			var newHTML = "<a class='list-group-item book'><h4 class='title'>"+books[i].name+status+"</h4><p class='author'><b>Author</b>&nbsp;&nbsp;"+books[i].author+"</p><div class='category'><b>Categories</b>&nbsp;&nbsp;"+books[i].catagory+"</div></a>";
+			var newHTML = "<a class='list-group-item book'><h4 class='title'>"+equipments[i].name+status+"</h4><p class='desc'><b>Description</b>&nbsp;&nbsp;"+equipments[i].description+"</p><div class='category'><b>Categories</b>&nbsp;&nbsp;"+equipments[i].catagory+"</div></a>";
 			renderedHTML += newHTML;
 		}
 	}
